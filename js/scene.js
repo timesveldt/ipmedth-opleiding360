0f93f1435173d8a4fe4ad9282a9db504a84cd2eb
Tim <timesveldt@hotmail.com>
2023-05-26 15:47:38 +0200

    Removed old prototype code

--- js/scene.js
+++ js/scene.js
@@ -1,9 +1,3 @@
-
-const achievementOne = document.getElementById('achievement--1');
-const achievementTwo = document.getElementById('achievement--2');
-const unlockedAchievement = document.getElementById('unlocked--achievement');
-const unlockedAchievementTitle = document.getElementById('unlocked--achievement--title');
-
 const myInfoSpot1 = document.getElementById('infospot--1');
 
 const infoSpot1 = new PANOLENS.Infospot();
@@ -13,8 +7,15 @@ infoSpot1.addHoverElement(myInfoSpot1, 150);
 const vrbutton = document.getElementById('vr-button');
 
 var place = document.getElementById('place');
-const locationInfo = document.getElementById("informatie");
-const information = ["dit is buiten voor de hoofdingang", "dit is waar je binnenkomt via de hoofdingang, Links zit de kantine", "Hier zit het Café waar je wat drinken kan bestellen", "Dit is de grote hal van de nieuwbouw", "dit is een studieruimte", "hier zitten de lokalen van de opleiding informatica"]
+const locationInfo = document.getElementById('informatie');
+const information = [
+    'dit is buiten voor de hoofdingang',
+    'dit is waar je binnenkomt via de hoofdingang, Links zit de kantine',
+    'Hier zit het Café waar je wat drinken kan bestellen',
+    'Dit is de grote hal van de nieuwbouw',
+    'dit is een studieruimte',
+    'hier zitten de lokalen van de opleiding informatica',
+];
 //panolens select container
 
 //init externat constances
@@ -37,7 +38,6 @@ const viewer = new PANOLENS.Viewer({
     container: pan,
 });
 
-
 var vrtoggled = false;
 
 // panorama2.setLinkingImage('./img/img1.png');
@@ -55,71 +55,6 @@ function vrtoggle() {
 
 vrbutton.addEventListener('click', vrtoggle);
 
-const panoramas = [
-    'panorama 1',
-    'panorama 2',
-    'panorama 3',
-    'panorama 4',
-    'panorama 5',
-    'panorama 6',
-];
-const visitedPanoramas = [];
-
-panorama.addEventListener('click', () => {
-    const date = new Date();
-    console.log(`[${date.toLocaleString()}]: click event panorama 1`);
-    visitedPanoramas.push('panorama 1');
-});
-
-panorama2.addEventListener('click', () => {
-    const date = new Date();
-    console.log(`[${date.toLocaleString()}]: click event panorama 2`);
-    visitedPanoramas.push('panorama 2');
-});
-
-panorama3.addEventListener('click', () => {
-    const date = new Date();
-    console.log(`[${date.toLocaleString()}]: click event panorama 3`);
-
-    unlockedAchievementTitle.textContent = 'Prestatie 2 ontgrendeld!';
-    unlockedAchievement.classList.add('achievement--unlocked');
-    setTimeout(() => {
-        unlockedAchievement.classList.remove('achievement--unlocked');
-    }, 3000);
-
-    achievementTwo.classList.add('achievements__achievement--unlocked');
-    visitedPanoramas.push('panorama 3');
-});
-
-panorama4.addEventListener('click', () => {
-    const date = new Date();
-    console.log(`[${date.toLocaleString()}]: click event panorama 4`);
-    visitedPanoramas.push('panorama 4');
-});
-
-panorama5.addEventListener('click', () => {
-    const date = new Date();
-    console.log(`[${date.toLocaleString()}]: click event panorama 5`);
-    visitedPanoramas.push('panorama 5');
-});
-
-panorama6.addEventListener('click', () => {
-    const date = new Date();
-    console.log(`[${date.toLocaleString()}]: click even panorama 6`);
-    visitedPanoramas.push('panorama 6');
-
-    visitedPanoramas.every((visitedPanorama) => {
-        if (panoramas.indexOf(visitedPanorama, -1)) {
-            unlockedAchievementTitle.textContent = 'Prestatie 1 ontgrendeld!';
-            unlockedAchievement.classList.add('achievement--unlocked');
-            setTimeout(() => {
-                unlockedAchievement.classList.remove('achievement--unlocked');
-            }, 3000);
-
-            achievementOne.classList.add('achievements__achievement--unlocked');
-        }
-    });
-});
 //linking between panorama's
 panorama.link(panorama2, new THREE.Vector3(5000, 200, -400), 500);
 panorama2.link(panorama, new THREE.Vector3(-5000, 100, 0), 500);
@@ -144,22 +79,22 @@ console.log(viewer);
 for (let index = 0; index < viewer.scene.children.length; index++) {
     switch (index) {
         case 0:
-            viewer.scene.children[index].name = "Buiten";
+            viewer.scene.children[index].name = 'Buiten';
             break;
         case 1:
-            viewer.scene.children[index].name = "Hoofdingang";
+            viewer.scene.children[index].name = 'Hoofdingang';
             break;
         case 2:
-            viewer.scene.children[index].name = "Café";
+            viewer.scene.children[index].name = 'Café';
             break;
         case 3:
-            viewer.scene.children[index].name = "Hal";
+            viewer.scene.children[index].name = 'Hal';
             break;
         case 4:
-            viewer.scene.children[index].name = "Studie ruimte";
+            viewer.scene.children[index].name = 'Studie ruimte';
             break;
         case 5:
-            viewer.scene.children[index].name = "Lokalen";
+            viewer.scene.children[index].name = 'Lokalen';
             break;
         default:
             break;
@@ -178,9 +113,6 @@ this.setInterval(() => {
 const startButton = document.querySelector('#startButton');
 const startScherm = document.querySelector('#startScherm');
 
-
 startButton.addEventListener('click', () => {
-    startScherm.classList.add("hidden");
-  })
-
-
+    startScherm.classList.add('hidden');
+});
